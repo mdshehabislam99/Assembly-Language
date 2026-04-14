fe53c4d37d82fb35a2a8651e002001ea5e6e108d
shehabislam99 <shihabkhanahab@gmail.com>
2026-04-15 03:49:29 +0530

    greater jump

--- compare.asm
+++ compare.asm
@@ -1,33 +1,50 @@
 dosseg
 .model small
 .stack 100h
+
 .data
+msg db 'Equal$'
+msg1 db 'greater$'
+var1 db ?
+var2 db ?
+
 .code 
 main proc
+mov ax,@data
+mov ds,ax
 
 mov ah,1
 int 21h
+mov var1,al
+mov bl,var1
 
-sub al,48
-mov dl,5
 
-cmp al,dl
+mov ah,1
+int 21h
+mov var2,al
+mov dl,var2
+
+cmp bl,dl
 je L1
-jl l2
+jg l2
 
-mov dl,'g'
+mov dl,'n'
 mov ah,2
 int 21h
 jmp L3
 
 L1:
-mov dl, 'e'
+lea dx,msg
+mov ah,9h
+int 21h
 mov ah,2
 int 21h
 jmp L3
 
 L2:
-mov dl,'l'
+lea dx,msg1
+mov ah,9h
+int 21h
 mov ah,2
 int 21h
 
--- loop.asm
+++ loop.asm
@@ -1,16 +1,23 @@
 dosseg
 .model small
 .stack 100h
+
 .data
+msg db 'Hello$'
+
 .code
 main proc
+mov ax,@data
+mov ds,ax
 
-mov cx,10
-l1:
-mov dx,'a'
+mov cx,9
+l2:
+lea dx,msg
+mov ah, 9h
+int 21h
 mov ah,2
 int 21h
-loop l1
+loop l2
 
 mov ah,4ch
 int 21h
